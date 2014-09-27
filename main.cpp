a6220b5d879b4e275fbd86dbe874da21c68aecd5
josejdlp <josejdlp@gmail.com>
2014-09-27 17:36:02 +0200

    Creacion del proyecto

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: josej
+ *
+ * Created on 27 de septiembre de 2014, 17:32
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
