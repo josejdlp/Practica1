4e36c870c8ad0da05ff5a50c2d8efe7b9b274829
josejdlp <josejdlp@gmail.com>
2014-09-29 18:30:24 +0200

    Creacion de clases vdin y errorRango y estructura del main try catch

--- main.cpp
+++ main.cpp
@@ -6,14 +6,44 @@
  */
 
 #include <cstdlib>
-
+#include <iostream>
+#include <fstream>
+#include "Vdin.h"
+#include "ErrorRango.h"
 using namespace std;
 
 /*
  * 
  */
+
 int main(int argc, char** argv) {
+    try{
+    
+        
+        
+        
+        
+        
+    //operamos con el vector
 
-    return 0;
+    //Realizamos borrados
+
+    //mostramos resultados
+
+
+    }catch(ErrorRango &e){
+         cout << e.what() << endl;
+    }
+    catch(bad_alloc e){
+     cout << "No hay memoria suficiente para el objeto dinamico" << endl;
+ }
+    
+ return 0;  
+
 }
+   
+   
 
+ void CargaFichero(){
+   
+}
